84e32ec1bd43a40253a4da8a79ad20418abd8a64
wookiespoo <jp862613@gmail.com>
2025-11-17 21:20:03 -0600

    Create vite.config.ts

---
+++ frontend/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
+  server: {
+    port: 3000,
+  },
+  define: {
+    'process.env': {}
+  }
+})
